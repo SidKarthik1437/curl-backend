e0ca93b1f0e13abe08ad228187c002cff505281c
SidKarthik <sa268592@gmail.com>
2022-01-22 14:54:35 +0530

    basic execution
    Co-authored-by: shreyas-sharma16 <shreyas-sharma16@users.noreply.github.com>
    Co-authored-by: Anjan Prasad <Anjanprasad112@users.noreply.github.com>
    Co-authored-by: Vijaya Vittala M <Vijayy08@users.noreply.github.com>

--- api/execution.py
+++ api/execution.py
@@ -2,22 +2,38 @@ import json
 from flask import jsonify
 from .functions import * 
 
-index = 0
+
 
 def execute(dump):
+    index = 0
+    exec = 0
     fdata = json.loads(json.dumps(dump))
     body = list(fdata.values())
     # data = json.dumps(body[1])
     a = dump['body']
     queue = a['queue']
+    exec = len(queue)
     data = a['data']
     print(queue)
     print(data)
     
-    global index
     
     for op in queue:
         if 'Add' in op:
             res, index = add(int(data[index]), int(data[index+1]), index)
             print(res)
             print(index)
+        elif 'Sub' in op:
+            res, index = sub(int(data[index]), int(data[index+1]), index)
+            print(res)
+            print(index)
+        elif 'Multiply' in op:
+            res, index = mul(int(data[index]), int(data[index+1]), index)
+            print(res)
+            print(index)
+        elif 'Divide' in op:
+            res, index = div(int(data[index]), int(data[index+1]), index)
+            print(res)
+            print(index)
+
+
--- api/functions.py
+++ api/functions.py
@@ -1,2 +1,8 @@
 def add(a, b, index):
     return a+b, index+2
+def sub(a, b, index):
+    return a-b, index+2
+def mul(a, b, index):
+    return a*b, index+2
+def div(a, b, index):
+    return a/b, index+2
